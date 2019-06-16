a6d1b5e975b9af867c77fd540c068db848ef0024
Renan Machado <renan.mav@hotmail.com>
2019-06-16 18:56:44 -0300

    Add global style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import './styles/global';
+
 function App() {
   return <div>Hello world</div>;
 }
---
+++ src/styles/colors.js
@@ -0,0 +1,21 @@
+export default {
+  white: '#FFF',
+  lighter: '#EEE',
+  light: '#DDD',
+  regular: '#999',
+  dark: '#666',
+  darker: '#333',
+  black: '#000',
+
+  primary: '#E63C3E',
+  secondary: '#FFC33D',
+
+  background: '#0B2131',
+
+  success: '#9DCA83',
+  danger: '#E37A7A',
+
+  transparent: 'transparent',
+  darkTransparent: 'rgba(0,0,0,0.6)',
+  whiteTransparent: 'rgba(255,255,255,0.3)',
+};
