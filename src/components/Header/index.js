25e271a75a053ac88a8bc10ed7955e38bc0610c5
Renan Machado <renan.mav@hotmail.com>
2019-06-18 11:01:07 -0300

    Split styles of header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,63 +1,8 @@
 import React from 'react';
 
-import styled from 'styled-components';
 import logo from '~/assets/logo.png';
 
-import { colors } from '~/styles';
-
-const Container = styled.div`
-  width: 100%;
-  height: 72px;
-  background: ${colors.background};
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const ContentWrapper = styled.div`
-  max-width: 1200px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  flex: 1;
-  padding: 0 20px;
-  color: ${colors.white};
-
-  img {
-    height: 32px;
-    width: 32px;
-    margin-right: 14px;
-  }
-
-  h1 {
-    font-weight: bold;
-    font-size: 18px;
-  }
-
-  h2 {
-    font-weight: bold;
-    font-size: 16px;
-    text-align: right;
-  }
-
-  a {
-    color: ${colors.white};
-    opacity: 0.6;
-    font-size: 14px;
-    text-align: right;
-    text-decoration: none;
-  }
-
-  div:nth-child(1) {
-    display: flex;
-    align-items: center;
-  }
-
-  div:nth-child(2) {
-    display: flex;
-    flex-direction: column;
-  }
-`;
+import { Container, ContentWrapper } from './styles';
 
 export default function Header() {
   return (
---
+++ src/components/Header/styles.js
@@ -0,0 +1,57 @@
+import styled from 'styled-components';
+
+import { colors } from '~/styles';
+
+export const Container = styled.div`
+  width: 100%;
+  height: 72px;
+  background: ${colors.background};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ContentWrapper = styled.div`
+  max-width: 1200px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex: 1;
+  padding: 0 20px;
+  color: ${colors.white};
+
+  img {
+    height: 32px;
+    width: 32px;
+    margin-right: 14px;
+  }
+
+  h1 {
+    font-weight: bold;
+    font-size: 18px;
+  }
+
+  h2 {
+    font-weight: bold;
+    font-size: 16px;
+    text-align: right;
+  }
+
+  a {
+    color: ${colors.white};
+    opacity: 0.6;
+    font-size: 14px;
+    text-align: right;
+    text-decoration: none;
+  }
+
+  div:nth-child(1) {
+    display: flex;
+    align-items: center;
+  }
+
+  div:nth-child(2) {
+    display: flex;
+    flex-direction: column;
+  }
+`;
