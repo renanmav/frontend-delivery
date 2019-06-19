e0fe31871a853d2a1fc93bc1e3d1f84a5750a32c
Renan Machado <renan.mav@hotmail.com>
2019-06-18 21:42:35 -0300

    Send action LOGIN_REQUEST

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { Types as AuthActions } from '~/store/ducks/auth';
 
 import { Background, AuthWrapper } from './styles';
 
 import logo from '~/assets/logo.png';
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    dispatch({
+      type: AuthActions.LOGIN_REQUEST,
+      payload: { email, password },
+    });
+  };
+
   return (
     <Background>
       <AuthWrapper>
         <img src={logo} alt="Logo" />
-        <form>
-          <input type="text" placeholder="Seu e-mail" />
-          <input type="password" placeholder="Senha secreta" />
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Seu e-mail"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Senha secreta"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
           <button type="submit">Entrar</button>
         </form>
       </AuthWrapper>
