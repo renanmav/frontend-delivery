3edd8c48a16e4f4ea92de19dc9e2be694b1c1ab1
Renan Machado <renan.mav@hotmail.com>
2019-06-19 19:44:27 -0300

    Redirect to orders

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,38 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import {
+  BrowserRouter, Switch, Route, Redirect,
+} from 'react-router-dom';
 
 import Login from './pages/Login';
 import Orders from './pages/Orders';
 
 export default function Routes() {
+  const { token } = useSelector(state => state.auth);
+
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={Login} />
-        <Route exact path="/dashboard" component={Orders} />
+        <Route
+          exact
+          path="/"
+          render={(routerProps) => {
+            if (token) {
+              return <Redirect to="/dashboard" />;
+            }
+            return <Login {...routerProps} />;
+          }}
+        />
+        <Route
+          exact
+          path="/dashboard"
+          render={(routerProps) => {
+            if (token) {
+              return <Orders {...routerProps} />;
+            }
+            return <Redirect to="/" />;
+          }}
+        />
       </Switch>
     </BrowserRouter>
   );
